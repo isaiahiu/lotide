cd6cd5c46098d67d161e53fbafd3bfe9c8225e84
Isaiah <isaiah.iu@hotmail.com>
2022-03-07 20:04:49 +0000

    adding first function activity

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  let failEmoji = String.fromCodePoint(0x1f6d1);
+  let passEmoji = String.fromCodePoint(0x2705);
+  actual === expected
+    ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
