8ce2ffd7f41ae490eb693279e0b370d08bea1f5e
Isaiah <isaiah.iu@hotmail.com>
2022-03-08 22:14:08 +0000

    solution to exercise

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function (firstArr, secArr) {
+  let failEmoji = String.fromCodePoint(0x1f6d1);
+  let passEmoji = String.fromCodePoint(0x2705);
+  console.log(
+    eqArrays(firstArr, secArr)
+      ? `${passEmoji} Assertion Passed`
+      : `${failEmoji} Assertion Failed`
+  );
+};
+
+assertArraysEqual([1, 2, "3"], [1, 2, 3]);
