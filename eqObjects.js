81e22d6794a558714948fea0e1eeac9b6a8e4f81
Isaiah <isaiah.iu@hotmail.com>
2022-03-09 20:31:03 +0000

    function failing as expected

--- eqObjects.js
+++ eqObjects.js
@@ -12,7 +12,7 @@ const eqObjects = function (object1, object2) {};
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // true
+assertEqual(eqObjects(ab, ba), true); // true
 
 const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // false
+assertEqual(eqObjects(ab, abc), true); // false
