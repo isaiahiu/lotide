06706853f04a9e898d5a07304a12e534ff824dd6
Isaiah <isaiah.iu@hotmail.com>
2022-03-09 20:52:19 +0000

    finished defining code will test case it

--- eqObjects.js
+++ eqObjects.js
@@ -6,9 +6,22 @@ const assertEqual = function (actual, expected) {
     : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {};
+const eqObjects = function (object1, object2) {
+  // scan both objects to see if they have same number of keys
+  // scan to see if keys are same name and have same value,
+  let objKeys1 = Object.keys(object1);
+  let objKeys2 = Object.keys(object2);
+  if (objKeys1.length === objKeys2.length) {
+    for (let keys of objKeys1) {
+      if (object1[keys] !== object2[keys]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
