59745fa22086601efccebb8d0d24b3e8aab0c2e8
Isaiah <isaiah.iu@hotmail.com>
2022-03-09 20:15:14 +0000

    removed console log and commented out test cases + eslint

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   let failEmoji = String.fromCodePoint(0x1f6d1);
   let passEmoji = String.fromCodePoint(0x2705);
   actual === expected
@@ -6,24 +6,22 @@ const assertEqual = function (actual, expected) {
     : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const findKeyByValue = function (obj, valu) {
-  // loop over the keys in obj
+const findKeyByValue = function(obj, valu) {
   for (let keys in obj) {
-    // if value matches the obj key value
     if (obj[keys] === valu) {
-      // return obj key
       return keys;
     }
   }
   return undefined;
-  // if no match, return undefined
 };
 
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  "sci fi": "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
+  horror: "Grudge",
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Grudge"), "horror");
