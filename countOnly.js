7b26e3ed04aa00bf8f48295667f93748ce71e0d1
Isaiah <isaiah.iu@hotmail.com>
2022-03-09 18:41:19 +0000

    adding object exercise

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function (actual, expected) {
+  let failEmoji = String.fromCodePoint(0x1f6d1);
+  let passEmoji = String.fromCodePoint(0x2705);
+  actual === expected
+    ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+  for (const items of allItems) {
+    if (itemsToCount[items]) {
+      if (results[items]) {
+        results[items] += 1;
+      } else {
+        results[items] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
