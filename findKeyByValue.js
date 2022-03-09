3cbb23bfae185150acf8893e07ca5f53106cf7c9
Isaiah <isaiah.iu@hotmail.com>
2022-03-09 20:07:08 +0000

    finalizing the function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  let failEmoji = String.fromCodePoint(0x1f6d1);
+  let passEmoji = String.fromCodePoint(0x2705);
+  actual === expected
+    ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function (obj, valu) {
+  // loop over the keys in obj
+  for (let keys in obj) {
+    // if value matches the obj key value
+    if (obj[keys] === valu) {
+      // return obj key
+      return keys;
+    }
+  }
+  // if no match, return undefined
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
