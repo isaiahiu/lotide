bed108ccea44a7f610f1f97390540aad3546adcd
Isaiah <isaiah.iu@hotmail.com>
2022-03-10 21:20:29 +0000

    implementing findkey function

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  let failEmoji = String.fromCodePoint(0x1f6d1);
+  let passEmoji = String.fromCodePoint(0x2705);
+  actual === expected
+    ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(obj, cb) {
+  for (let key in obj) {
+    if (cb(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const example = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+// assertEqual(example, "noma");
