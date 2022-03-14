8f01a0b925bf38dd9cb468aff4f5ed54e07b7fb0
Isaiah <isaiah.iu@hotmail.com>
2022-03-14 17:55:41 +0000

    refactored middle and adding middleTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,14 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArray = require("./eqArray");
 
-const assertArraysEqual = function(firstArr, secArr) {
+const assertArraysEqual = function (firstArr, secArr) {
   let failEmoji = String.fromCodePoint(0x1f6d1);
   let passEmoji = String.fromCodePoint(0x2705);
   console.log(
-    eqArrays(firstArr, secArr)
+    eqArray(firstArr, secArr)
       ? `${passEmoji} Assertion Passed`
       : `${failEmoji} Assertion Failed`
   );
 };
 
 // assertArraysEqual([1, 2, "3"], [1, 2, 3]);
+module.exports = assertArraysEqual;
--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,7 +1,7 @@
-const eqArrays = require("../eqArray");
+const eqArray = require("../eqArray");
 const assertEqual = require("../assertEqual");
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArray([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArray(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArray(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true); // => should PASS
