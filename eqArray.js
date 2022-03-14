92a646173bbdc35c1d5459981c534bcb2dd07e97
Isaiah <isaiah.iu@hotmail.com>
2022-03-14 17:20:20 +0000

    refactored eqArray and adding test file

--- eqArray.js
+++ eqArray.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  let failEmoji = String.fromCodePoint(0x1f6d1);
-  let passEmoji = String.fromCodePoint(0x2705);
-  actual === expected
-    ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require("./assertEqual");
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
@@ -19,11 +13,4 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-console.log("------------------");
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-console.log("------------------");
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-console.log("------------------");
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-console.log("------------------");
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
